d6d73cc0c71149fc2d3904d822f6b022997b021a
Kearo <drache1000@gmx.net>
2019-01-12 20:32:01 +0100

    test

---
+++ 2d movement tests/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 30f;
+    private int battleMode = 0;
+    public GameObject syncChar1;
+    public GameObject syncChar2;
+    public GameObject player;
+    bool mirrorMode = false;
+    public float maxDistance;
+    SyncCharMovement sync;
+    Rigidbody2D rb;
+    Vector2 moveDistance;
+    public CinemachineVirtualCamera vcam;
+
+    // Use this for initialization
+    void Start () {
+        syncChar2.SetActive(false);
+        syncChar1.SetActive(true);
+        sync = GetComponent<SyncCharMovement>();
+        rb = GetComponent<Rigidbody2D>();
+        Vector2 moveDistance = new Vector2(0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moveDistance = new Vector2(0, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane plane = new Plane(Vector3.forward, transform.position);
+            float dist = 0;
+            if(plane.Raycast(ray, out dist))
+            {
+                sync.UseAbility(ray.GetPoint(dist), mirrorMode);
+            }
+        } 
+
+        if (Input.GetKey("w"))
+        {             
+                Vector2 upMove = Vector2.up * speed * Time.deltaTime;
+                moveDistance = moveDistance + upMove;
+        }
+        if (Input.GetKey("s"))
+        {
+            Vector2 downMove = Vector2.down * speed * Time.deltaTime;
+            moveDistance = moveDistance + downMove;            
+        }
+        if (Input.GetKey("d"))
+        {
+            Vector2 rightMove = Vector2.right * speed * Time.deltaTime;
+                moveDistance = moveDistance + rightMove;
+        }
+        if (Input.GetKey("a"))
+        {
+            Vector2 leftMove = Vector2.left * speed * Time.deltaTime;    
+                moveDistance = moveDistance + leftMove;             
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            battleMode++;
+            if (battleMode > 3)
+            {
+                battleMode = 0;
+            }
+            switch (battleMode)
+            {
+                case 0:
+                    syncChar1.transform.position = new Vector2(syncChar1.transform.position.x + syncChar1.GetComponentInChildren<BoxCollider2D>().bounds.size.x / 2, syncChar1.transform.position.y);
+                    syncChar2.transform.position = syncChar1.transform.position;                    
+                    syncChar1.SetActive(true);
+                    syncChar2.SetActive(false);
+                    mirrorMode = false;
+                    break;
+                case 1:
+                    syncChar1.SetActive(false);
+                    syncChar2.SetActive(true);
+                    break;
+                case 2:
+                    syncChar1.SetActive(true);               
+                    syncChar2.transform.position = new Vector2(syncChar1.transform.position.x + syncChar1.GetComponentInChildren<BoxCollider2D>().bounds.size.x/2, syncChar1.transform.position.y);
+                    syncChar1.transform.position = new Vector2(syncChar1.transform.position.x - syncChar1.GetComponentInChildren<BoxCollider2D>().bounds.size.x / 2, syncChar1.transform.position.y);
+                    break;
+                case 3:
+                    mirrorMode = true;
+                    break;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!sync.GetActiveAbility())
+        {
+            if (!mirrorMode)
+            {
+                rb.MovePosition(rb.position + moveDistance);
+            }
+            else
+            {
+                //todo set rb
+                float distanceX = syncChar1.transform.position.x - rb.position.x;
+                float distanceY = syncChar1.transform.position.y - rb.position.y;
+                distanceX = Math.Abs(distanceX);
+                distanceY = Math.Abs(distanceY);             
+                if (distanceX < maxDistance && distanceY < maxDistance)
+                {
+                    syncChar1.transform.Translate(moveDistance);
+                    syncChar2.transform.Translate(moveDistance * -1f);
+                }
+                else
+                {
+                    //todo
+                    rb.MovePosition(rb.position+moveDistance);
+                    if (distanceX > maxDistance)
+                    {
+                        moveDistance.x = 0;
+                    }
+                    else
+                    {
+                        moveDistance.y = 0;
+                    }
+                    syncChar2.transform.Translate(moveDistance);
+                }        
+            }
+        }
+    }
+
+    public bool GetMirrorMode()
+    {
+        return mirrorMode;
+    }
+}
---
+++ 2d movement tests/Assets/Scripts/SyncCharMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncCharMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public float abilitySpeed = 30;
+    bool activeAbility = false;
+    bool mirrormode = false;
+    Vector3 mousePos;
+    Vector2 dir;
+    public GameObject sync1;
+    public GameObject sync2;
+    public float lerpPrescicion = 0.1f;
+    Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (activeAbility)
+        {
+            // transform.position = Vector3.Lerp(transform.position, mousePos, abilitySpeed * Time.deltaTime);
+            rb.MovePosition(rb.position + dir.normalized / abilitySpeed);
+
+            if (mirrormode)
+            {//todo
+                sync1.transform.position = Vector3.Lerp(sync1.transform.position, new Vector3(rb.position.x, rb.position.y, 0) - sync1.GetComponent<BoxCollider2D>().bounds.size / 2, abilitySpeed * Time.fixedDeltaTime);
+                sync2.transform.position = Vector3.Lerp(sync2.transform.position, new Vector3(rb.position.x, rb.position.y, 0) + sync2.GetComponent<BoxCollider2D>().bounds.size / 2, abilitySpeed * Time.fixedDeltaTime);
+            }
+            //todo
+            Debug.Log(Mathf.Abs(dir.x - rb.position.x)< lerpPrescicion);
+            Debug.Log(Mathf.Abs(dir.y - rb.position.y));
+            if (Mathf.Abs(dir.x - rb.position.x) < lerpPrescicion && Mathf.Abs(dir.y + rb.position.y) < lerpPrescicion)
+            {
+                Debug.Log("REEE");
+                activeAbility = false;
+            }        
+        }
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+          activeAbility = false;
+    }
+
+    public void UseAbility(Vector3 mousePosition, bool mirror)
+    {
+        mirrormode = mirror;
+        mousePos = mousePosition;
+        activeAbility = true;
+        dir = mousePos;
+        dir = dir - rb.position;
+    }
+
+    public bool GetActiveAbility()
+    {
+        return activeAbility;
+    }
+}
